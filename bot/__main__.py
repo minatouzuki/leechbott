d14139af1623488a4e893bd012d20655f1936bf6
minatouzuki <72920726+minatouzuki@users.noreply.github.com>
2021-01-28 12:15:49 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,6 +1,6 @@
 from os.path import join as os_path_join
 from pyrogram import Client, filters
-from pyrogram.types import Message, CallbackQueryHandler
+from pyrogram.types import Message
 from bot import CONFIG, COMMAND, LOCAL, LOGGER, STATUS
 from bot.handlers import *
 import asyncio
