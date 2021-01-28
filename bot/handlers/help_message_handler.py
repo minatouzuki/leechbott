1157fb80088882103b2ffbea62a0ab4ff92423a8
minatouzuki <72920726+minatouzuki@users.noreply.github.com>
2021-01-28 12:27:55 +0530

    Update help_message_handler.py

--- bot/handlers/help_message_handler.py
+++ bot/handlers/help_message_handler.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters
 from pyrogram.types import Message 
 from bot import COMMAND, LOCAL
 
-@Client.on_message(Filters.command(COMMAND.HELP))
+@Client.on_message(filters.command(COMMAND.HELP))
 async def func(client : Client, message: Message):
     text = LOCAL.HELP_MESSAGE_HEADER + "\n"
     for cmd_code, cmd in COMMAND:
