d9a164f2b86b9211b4f25af61ef8a8338f497852
minatouzuki <72920726+minatouzuki@users.noreply.github.com>
2021-01-28 12:27:24 +0530

    Update default_message_handler.py

--- bot/handlers/default_message_handler.py
+++ bot/handlers/default_message_handler.py
@@ -1,8 +1,9 @@
-from pyrogram import Client, Message, Filters
+from pyrogram import Client, filters
+from pyrogram.types import Message 
 from bot import COMMAND
 from bot.handlers import leech_handler
 
-@Client.on_message(Filters.private & ~Filters.regex(r'^/'))
+@Client.on_message(filters.private & ~filters.regex(r'^/'))
 async def func(client : Client, message : Message):
     message.text = "/" + COMMAND.LEECH + " " + message.text
     return await leech_handler.func(client, message)
