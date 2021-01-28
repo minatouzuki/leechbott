d8e07964a12b9cc6d24382dd83e41557a1b801ac
minatouzuki <72920726+minatouzuki@users.noreply.github.com>
2021-01-28 12:37:45 +0530

    Update wrong_room_handler.py

--- bot/handlers/wrong_room_handler.py
+++ bot/handlers/wrong_room_handler.py
@@ -1,4 +1,5 @@
-from pyrogram import Client, Message
+from pyrogram import Client
+from pyrogram.types import Message 
 from bot import LOCAL, CONFIG
 
 async def func(client : Client, message: Message):
