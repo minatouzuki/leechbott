34ed350f1439a947a399d7cdd4d7d0f2b2b2f77c
minatouzuki <72920726+minatouzuki@users.noreply.github.com>
2021-01-28 12:35:54 +0530

    Update upload_as_doc_handler.py

--- bot/handlers/upload_as_doc_handler.py
+++ bot/handlers/upload_as_doc_handler.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters
 from pyrogram.types import Message 
 from bot import LOCAL, STATUS, COMMAND
 
-@Client.on_message(Filters.command(COMMAND.UPLOAD_AS_DOC))
+@Client.on_message(filters.command(COMMAND.UPLOAD_AS_DOC))
 async def func(client : Client, message: Message):
     STATUS.UPLOAD_AS_DOC = not STATUS.UPLOAD_AS_DOC
     await message.reply_text(LOCAL.UPLOAD_AS_DOC.format(status=STATUS.UPLOAD_AS_DOC))
