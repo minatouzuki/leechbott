49b89605288cebf1b1906ac6be35f9fddcb6460f
minatouzuki <72920726+minatouzuki@users.noreply.github.com>
2021-01-28 12:34:22 +0530

    Update thumbnail_handler.py

--- bot/handlers/thumbnail_handler.py
+++ bot/handlers/thumbnail_handler.py
@@ -1,11 +1,12 @@
-from pyrogram import Client, Message, Filters
+from pyrogram import Client, filters
+from pyrogram.types import Message 
 from os.path import join as os_path_join
 from bot import COMMAND, LOCAL, CONFIG
 from bot.plugins import thumbnail_video
 
 thumbnail_path = os_path_join(CONFIG.ROOT, CONFIG.WORKDIR, CONFIG.THUMBNAIL_NAME)
 
-@Client.on_message(Filters.command(COMMAND.SET_THUMBNAIL))
+@Client.on_message(filters.command(COMMAND.SET_THUMBNAIL))
 async def set(client : Client, message: Message):
     if not message.photo:
         return await message.reply_text(LOCAL.THUMBNAIL_NO_PHOTO.format(cmd_set_thumbnail = COMMAND.SET_THUMBNAIL))
@@ -17,7 +18,7 @@ async def set(client : Client, message: Message):
     await thumbnail_video.set(thumbnail_path)
     await reply.edit_text(LOCAL.THUMBNAIL_APPLIED)
 
-@Client.on_message(Filters.command(COMMAND.RESET_THUMBNAIL))
+@Client.on_message(filters.command(COMMAND.RESET_THUMBNAIL))
 async def reset(client : Client, message: Message):
     reply = await message.reply_text(LOCAL.THUMBNAIL_DELETING)
     await thumbnail_video.reset(thumbnail_path)
