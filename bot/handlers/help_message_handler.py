9684393bb639bedf8bf7e6880082b7b4a89c294a
minatouzuki <72920726+minatouzuki@users.noreply.github.com>
2021-01-28 12:24:24 +0530

    Update help_message_handler.py

--- bot/handlers/help_message_handler.py
+++ bot/handlers/help_message_handler.py
@@ -1,4 +1,5 @@
-from pyrogram import Client, Message, Filters
+from pyrogram import Client, filters
+from pyrogram.types import Message 
 from bot import COMMAND, LOCAL
 
 @Client.on_message(Filters.command(COMMAND.HELP))
