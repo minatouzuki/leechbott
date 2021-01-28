7ec4fb248ce88fffb9faadecda8f5c73ff8672e8
minatouzuki <72920726+minatouzuki@users.noreply.github.com>
2021-01-28 12:22:53 +0530

    Update upload_as_zip_handler.py

--- bot/handlers/upload_as_zip_handler.py
+++ bot/handlers/upload_as_zip_handler.py
@@ -1,4 +1,5 @@
-from pyrogram import Client, Message, Filters
+from pyrogram import Client, filters
+from pyrogram.types import Message 
 from bot import LOCAL, STATUS, COMMAND
 
 @Client.on_message(Filters.command(COMMAND.UPLOAD_AS_ZIP))
